3c3fbfadd0011c484f07175fca57334a4704cc72
YES-Tanikawa <s1280208@u-aizu.ac.jp>
2022-02-18 22:29:00 +0900

    getFollowesのcontrollerの実装

--- controller/friends.go
+++ controller/friends.go
@@ -10,12 +10,13 @@ import (
 type FriendsController struct {
 	ujs service.UserJWTService
 	fsu usecase.FriendsSearchUseCase
+	gfu usecase.GetFolloweesUseCase
 }
 
 func convertToJson(twitterUser *model.TwitterUser) *TwitterUser {
 	return &TwitterUser{
 		ID:          twitterUser.ID,
-		ScreenNme:   twitterUser.ScreenName,
+		ScreenName:   twitterUser.ScreenName,
 		DisplayName: twitterUser.DisplayName,
 		ImageUrl:    twitterUser.ProfileImageUrl,
 		Biography:   twitterUser.Biography,
@@ -52,9 +53,39 @@ func (f *FriendsController) FriendsSearch(c *gin.Context) {
 	c.JSON(200, jsonArray)
 }
 
-func NewFriendsController(ujs service.UserJWTService, fsu usecase.FriendsSearchUseCase) *FriendsController {
+func (f *FriendsController) GetFollowees(c *gin.Context){
+	token, err := GetAuthToken(c)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	userID, err := f.ujs.GetUserIDFromJWT(token)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	searchResult, err := f.gfu.Execute(userID)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	jsonArray := make([]*TwitterUser, len(searchResult))
+	for i, user := range searchResult {
+		jsonArray[i] = convertToJson(user)
+	}
+	c.JSON(200, jsonArray)
+}
+
+func NewFriendsController(ujs service.UserJWTService, fsu usecase.FriendsSearchUseCase, gfu usecase.GetFolloweesUseCase) *FriendsController {
 	return &FriendsController{
 		ujs: ujs,
 		fsu: fsu,
+		gfu: gfu,
 	}
 }
