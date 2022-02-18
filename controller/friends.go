bf8d2adf134fbcb1f8034a38520c803f350af1d5
yt8492 <tmymyut2000@gmail.com>
2022-02-19 01:39:10 +0900

    POST /friends/:id の実装

--- controller/friends.go
+++ controller/friends.go
@@ -5,11 +5,13 @@ import (
 	"github.com/Zli-UoA/ryouomoi-checker-backend/service"
 	"github.com/Zli-UoA/ryouomoi-checker-backend/usecase"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 type FriendsController struct {
 	ujs service.UserJWTService
 	fsu usecase.FriendsSearchUseCase
+	slpu usecase.SetLovePointUseCase
 }
 
 func convertToJson(twitterUser *model.TwitterUser) *TwitterUser {
@@ -52,9 +54,51 @@ func (f *FriendsController) FriendsSearch(c *gin.Context) {
 	c.JSON(200, jsonArray)
 }
 
-func NewFriendsController(ujs service.UserJWTService, fsu usecase.FriendsSearchUseCase) *FriendsController {
+func (f *FriendsController) SetLovePoint(c *gin.Context) {
+	token, err := GetAuthToken(c)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	userID, err := f.ujs.GetUserIDFromJWT(token)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	loverUserIDStr := c.Param("id")
+	loverUserID, err := strconv.ParseInt(loverUserIDStr, 10, 64)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	req := &LovePoint{}
+	err = c.ShouldBindJSON(req)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": err.Error(),
+		})
+	}
+	matchSuccess, err := f.slpu.Execute(userID, loverUserID, req.LovePoint)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	res := MatchResult{MatchSuccess: matchSuccess}
+	c.JSON(200, res)
+}
+
+func NewFriendsController(ujs service.UserJWTService, fsu usecase.FriendsSearchUseCase, slpu usecase.SetLovePointUseCase) *FriendsController {
 	return &FriendsController{
 		ujs: ujs,
 		fsu: fsu,
+		slpu: slpu,
 	}
 }
